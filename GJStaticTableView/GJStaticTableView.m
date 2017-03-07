d000ab447a15a32a7be06c4356dd9f9e45e1b47d
wangyutao <yutaowang@creditease.cn>
2017-03-07 12:52:18 +0800

    v0.0.1

--- GJStaticTableView/GJStaticTableView.m
+++ GJStaticTableView/GJStaticTableView.m
@@ -45,35 +45,28 @@ BOOL canResponseProtocolMethod(id obj, Protocol *protocol, SEL selector) {
 - (void)loadCells {
     Class cls = self.class;
     
-//    while (![cls isEqual:GJStaticTableView.class]) {
-        unsigned count = 0;
-        objc_property_t *properties = class_copyPropertyList(cls, &count);
-        
-//        if (properties == NULL) continue;
+    NSMutableArray *classList = @[].mutableCopy;
+    while (![cls isEqual:GJStaticTableView.class]) {
+        [classList insertObject:cls atIndex:0];
+        cls = cls.superclass;
+    }
     
+    for (Class class in classList) {
+        unsigned count = 0;
+        objc_property_t *properties = class_copyPropertyList(class, &count);
+        if (properties == NULL) continue;
+        
         for (unsigned i = 0; i < count; i++) {
             objc_property_t property = properties[i];
             [self loadValidProperty:property];
         }
         free(properties);
-        cls = cls.superclass;
-//    }
+    }
 }
 
-//R	The property is read-only (readonly).
-//C	The property is a copy of the value last assigned (copy).
-//&	The property is a reference to the value last assigned (retain).
-//N	The property is non-atomic (nonatomic).
-//G<name>	The property defines a custom getter selector name. The name follows the G (for example, GcustomGetter,).
-//S<name>	The property defines a custom setter selector name. The name follows the S (for example, ScustomSetter:,).
-//D	The property is dynamic (@dynamic).
-//W	The property is a weak reference (__weak).
-//P	The property is eligible for garbage collection.
-//t<encoding>	Specifies the type using old-style encoding.
 - (void)loadValidProperty:(objc_property_t)property {
     unsigned int attrCount;
     objc_property_attribute_t *attrs = property_copyAttributeList(property, &attrCount);
-    Class cls;
     NSString *typeEncoding;
     const char *name = property_getName(property);
     NSString *nameStr;
@@ -82,38 +75,60 @@ BOOL canResponseProtocolMethod(id obj, Protocol *protocol, SEL selector) {
     }
     if (!nameStr) return;
     
-    for (unsigned int i = 0; i < attrCount; i++) {
-        if (attrs[i].name[0] == 'T') {
-            if (attrs[i].value) {
+    Class cls;
+    SEL setter = NULL;
+    NSString *clsName;
+
+    for (unsigned int i = 0; i < attrCount; i++) {//refrence from YY
+        switch (attrs[i].name[0]) {
+            case 'T': {
                 typeEncoding = [NSString stringWithUTF8String:attrs[i].value];
                 
                 if (typeEncoding.length) {
                     NSScanner *scanner = [NSScanner scannerWithString:typeEncoding];
                     if (![scanner scanString:@"@\"" intoString:NULL]) continue;
                     
-                    NSString *clsName = nil;
                     if ([scanner scanUpToCharactersFromSet: [NSCharacterSet characterSetWithCharactersInString:@"\"<"] intoString:&clsName]) {
                         if (clsName.length) cls = objc_getClass(clsName.UTF8String);
-                        if ([cls instancesRespondToSelector:@selector(initWithStyle:reuseIdentifier:)]) {
-                            SEL setter = NSSelectorFromString([NSString stringWithFormat:@"set%@%@:", [nameStr substringToIndex:1].uppercaseString, [nameStr substringFromIndex:1]]);
-                            id cell;
-                            NSString *path = [[NSBundle mainBundle] pathForResource:clsName ofType:@"nib"];
-                            if (path) {
-                                cell = [[NSBundle mainBundle] loadNibNamed:clsName owner:nil options:0].firstObject;
-                                [self registerNib:[UINib nibWithNibName:clsName bundle:nil] forCellReuseIdentifier:clsName];
-                            }
-                            if (!cell) {
-                                cell = [[cls alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:clsName];
-                                [self registerClass:cls forCellReuseIdentifier:clsName];
-                            }
-                            ((void (*)(id, SEL, id))(void *) objc_msgSend)(self, setter, (id)cell);
-                            [self._cells addObject:cell];
-                        }
                     }
                 }
-            }
+            } break;
+            
+            case 'S': {
+                if (attrs[i].value) {
+                    setter = NSSelectorFromString([NSString stringWithUTF8String:attrs[i].value]);
+                }
+            } break;
         }
     }
+    
+    if (!setter) {
+        setter = NSSelectorFromString([NSString stringWithFormat:@"set%@%@:", [nameStr substringToIndex:1].uppercaseString, [nameStr substringFromIndex:1]]);
+    }
+    
+    if (!setter) return;//no setter
+    
+    id cell;
+    if ([cls instancesRespondToSelector:@selector(initWithStyle:reuseIdentifier:)]) {
+        NSString *path = [[NSBundle mainBundle] pathForResource:clsName ofType:@"nib"];
+        //regist for FD
+        if (path) {
+            cell = [[NSBundle mainBundle] loadNibNamed:clsName owner:nil options:0].firstObject;
+            [self registerNib:[UINib nibWithNibName:clsName bundle:nil] forCellReuseIdentifier:clsName];
+        }
+        if (!cell) {
+            cell = [[cls alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:clsName];
+            [self registerClass:cls forCellReuseIdentifier:clsName];
+        }
+    }
+    
+    if (cell) {
+        if ([self respondsToSelector:setter]) {
+            ((void (*)(id, SEL, id))(void *) objc_msgSend)(self, setter, (id)cell);
+            [self._cells addObject:cell];
+        }
+    }
+    
     if (attrs) {
         free(attrs);
         attrs = NULL;
